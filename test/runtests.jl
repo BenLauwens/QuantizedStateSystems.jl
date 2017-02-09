716fe2c172c5af02445b8a66192c4e21ce07b2ba
Ben Lauwens <ben.lauwens@gmail.com>
2017-02-09 21:20:57 +0100

    Add yield to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ testpath(f) = joinpath(dirname(@__FILE__), f)
 for test_file in [
   "iterator_example.jl",
   "statefunctions.jl",
-  "closure.jl",]
+  "closure.jl",
+  "yield.jl",]
   include(testpath(test_file))
 end
